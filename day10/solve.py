b7c7f108730eda6ae1db02a8f069f4c58e8b6a78
Mathias Hatlestad <mathias.hatlestad@bidbax.no>
2025-12-11 20:27:28 +0100

    wip7

---
+++ day10/solve.py
@@ -0,0 +1,23 @@
+"""
+Advent of Code 2025 - Day 10
+"""
+
+import sys; from pathlib import Path; sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+from utils.aoc import run_solution
+
+
+def part1(data: list[str]) -> int:
+    """Solve part 1 of the puzzle."""
+    # TODO: Implement solution
+    return 0
+
+
+def part2(data: list[str]) -> int:
+    """Solve part 2 of the puzzle."""
+    # TODO: Implement solution
+    return 0
+
+
+if __name__ == '__main__':
+    run_solution(part1, part2, transform_input=lambda x: x.split('\n'))
+
